520ddbd5db408b4573d52ea04a5c920dd139f9c2
GremlinBabby <camillamhansen@outlook.com>
2018-02-23 17:50:12 +0100

    Button added with a listener (code missing for onClick) + temp. ViewAnimator field because I dunno what we'll use for the dog animation

--- app/src/main/java/com/example/camil/tabletapp/MainActivity.java
+++ app/src/main/java/com/example/camil/tabletapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.camil.tabletapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = findViewById(R.id.phoneLock);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Code for locking phones
+            }
+        });
+
+
     }
+
+
 }
