afb5ae5e721a65b57274d5275d917b7556a66380
KLS\karatinka <sakacova.katarina@gmail.com>
2018-03-13 10:09:04 +0100

    changing overall phone status to true; timeStamp created, when pushing the button; TimeStamp list created (logging all timestamps)

--- app/src/main/java/com/example/camil/tabletapp/MainActivity.java
+++ app/src/main/java/com/example/camil/tabletapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.camil.tabletapp;
 
 import android.graphics.drawable.AnimationDrawable;
+import android.nfc.FormatException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,8 +13,17 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MainActivity extends AppCompatActivity {
@@ -21,11 +31,16 @@ public class MainActivity extends AppCompatActivity {
     //connection to the firebase
     DatabaseReference databaseCode;
     DatabaseReference databaseTotal;
+    DatabaseReference databasePhoneStatus;
+    DatabaseReference databaseTimeStamp;
+    DatabaseReference databaseTimeLocked;
+    DatabaseReference databaseListTimeStamps;
 
     TextView totalCountTextView;
     String totalScoreString;
     int totalScoreInt;
     ImageView background;
+    String timestampListID;
 
 
     //string containing code for unlocking
@@ -40,9 +55,14 @@ public class MainActivity extends AppCompatActivity {
         //initiating the database
         databaseCode = FirebaseDatabase.getInstance().getReference("Code");
         databaseTotal = FirebaseDatabase.getInstance().getReference("Total");
+        databasePhoneStatus = FirebaseDatabase.getInstance().getReference("PhoneLockStatus");
+        databaseTimeStamp = FirebaseDatabase.getInstance().getReference("TimeStamp");
+        databaseTimeLocked = FirebaseDatabase.getInstance().getReference("TimesLockActivated");
+        databaseListTimeStamps = FirebaseDatabase.getInstance().getReference("ListTimeStamps");
+
+
 
         //method for checking the total count of codes entered
-
         super.onStart();
         databaseTotal.addValueEventListener(new ValueEventListener() {
             @Override
@@ -51,21 +71,21 @@ public class MainActivity extends AppCompatActivity {
                 totalScoreInt = Integer.parseInt(totalScoreString);
                 totalCountTextView.setText(String.valueOf(totalScoreInt));
                 //changing the animation
-                if (totalScoreInt >= 0) {
+                if (totalScoreInt >= 100) {
                     ImageView stageOne = findViewById(R.id.dogStateImageView);
                     stageOne.setImageResource(R.drawable.stageone);
                     AnimationDrawable stageOneAnimation = (AnimationDrawable) stageOne.getDrawable();
                     stageOneAnimation.start();
                     background = findViewById(R.id.backgroundImageView);
                     background.setImageResource(R.drawable.firststage);
-                } else if (totalScoreInt < 0 && totalScoreInt >= -3) {
+                } else if (totalScoreInt < 100 && totalScoreInt >= 97) {
                     ImageView stageTwo = findViewById(R.id.dogStateImageView);
                     stageTwo.setImageResource(R.drawable.stagetwo);
                     AnimationDrawable stageTwoAnimation = (AnimationDrawable) stageTwo.getDrawable();
                     stageTwoAnimation.start();
                     background = findViewById(R.id.backgroundImageView);
                     background.setImageResource(R.drawable.secondstage);
-                } else if (totalScoreInt < -3 && totalScoreInt >= -5) {
+                } else if (totalScoreInt < 97 && totalScoreInt >= 95) {
                     ImageView stageThree = findViewById(R.id.dogStateImageView);
                     stageThree.setImageResource(R.drawable.stagethree);
                     AnimationDrawable stageThreeAnimation = (AnimationDrawable) stageThree.getDrawable();
@@ -87,13 +107,17 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-
-
+        //pushing the button LockPhones
         final Button button = findViewById(R.id.phoneLockButton);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Code for locking phones
+                //Code for locking phones and storing timestamp of locking the phones
+                changeToTrue();
+                databaseTimeStamp.setValue(ServerValue.TIMESTAMP);
+                timestampListID = databaseListTimeStamps.push().getKey();
+                databaseListTimeStamps.child(timestampListID).setValue(ServerValue.TIMESTAMP);
+
 
                 //To generate code - targets API 21
                 CodeGenerator codeGenerator = new CodeGenerator(6, ThreadLocalRandom.current());
@@ -120,4 +144,11 @@ public class MainActivity extends AppCompatActivity {
     public void addCode(String unlockcode) {
         databaseCode.setValue(unlockcode);
     }
+
+    //changing phoneLockStatus to TRUE
+    public void changeToTrue() {
+        databasePhoneStatus.setValue(true);
+    }
+
 }
+
